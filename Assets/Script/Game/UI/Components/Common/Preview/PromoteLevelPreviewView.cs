892da59abbb1165441ca0e6add184c1aee3a5ff5
SOLnene <664621381@qq.com>
2026-05-09 02:07:49 +0800

    添加promotepreviewview(待测试)

---
+++ Assets/Script/Game/UI/Components/Common/Preview/PromoteLevelPreviewView.cs
@@ -0,0 +1,106 @@
+using Cysharp.Threading.Tasks;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PromoteLevelPreviewView : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI currentLevelText;
+    [SerializeField]
+    TextMeshProUGUI nextLevelText;
+    [SerializeField]
+    GameObject currentStarContent;
+    [SerializeField]
+    GameObject nextStarContent;
+    [SerializeField]
+    AnimatedPanel animatedPanel;
+
+    readonly CompositeDisposable disposable = new();
+    PromoteLevelPreviewViewModel vm;
+
+    public void Bind(PromoteLevelPreviewViewModel viewModel)
+    {
+        disposable.Clear();
+        vm = viewModel;
+
+        if (vm == null)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+
+        vm.currentLevelText
+            .Subscribe(text =>
+            {
+                if (currentLevelText != null)
+                    currentLevelText.text = text;
+            })
+            .AddTo(disposable);
+
+        vm.nextLevelText
+            .Subscribe(text =>
+            {
+                if (nextLevelText != null)
+                    nextLevelText.text = text;
+            })
+            .AddTo(disposable);
+
+        vm.maxRanked
+            .Subscribe(maxRanked =>
+            {
+                if (nextLevelText != null)
+                    nextLevelText.gameObject.SetActive(!maxRanked);
+
+                if (nextStarContent != null)
+                    nextStarContent.SetActive(!maxRanked);
+            })
+            .AddTo(disposable);
+
+        vm.currentStarCount
+            .Subscribe(count => SetStars(currentStarContent, count))
+            .AddTo(disposable);
+
+        vm.nextStarCount
+            .Subscribe(count => SetStars(nextStarContent, count))
+            .AddTo(disposable);
+    }
+
+    public async UniTask Show()
+    {
+        gameObject.SetActive(true);
+
+        if (animatedPanel != null)
+            await animatedPanel.Show();
+    }
+
+    public async UniTask Hide()
+    {
+        if (animatedPanel != null)
+        {
+            await animatedPanel.Hide();
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void SetStars(GameObject root, int count)
+    {
+        if (root == null)
+            return;
+
+        var icons = root.GetComponentsInChildren<Image>(true);
+        for (int i = 0; i < icons.Length; i++)
+        {
+            icons[i].color = i < count ? Color.white : Color.grey;
+        }
+    }
+
+    void OnDestroy()
+    {
+        disposable.Dispose();
+    }
+}
