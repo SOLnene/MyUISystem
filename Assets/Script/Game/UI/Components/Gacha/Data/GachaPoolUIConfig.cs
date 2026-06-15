2f9fb5f3857b441f971b029adc195101f3089104
SOLnene <664621381@qq.com>
2026-06-16 00:34:26 +0800

    修改uipoolconfig,添加mainsubicon，key和文本

--- Assets/Script/Game/UI/Components/Gacha/Data/GachaPoolUIConfig.cs
+++ Assets/Script/Game/UI/Components/Gacha/Data/GachaPoolUIConfig.cs
@@ -6,10 +6,17 @@ using UnityEngine.Serialization;
 [CreateAssetMenu(menuName = "Game/Gacha Pool UI Config")]
 public class GachaPoolUIConfig : ScriptableObject
 {
+    public string gachaKey;
     public GachaPoolType poolType;
     // 入口（必定存在）
     public Sprite tabIcon;
 
     // 内容（可失败）
-    public Sprite poolVisual;
+    public Sprite poolBackground;
+    [FormerlySerializedAs("poolVisual")]
+    public Sprite primaryRateUpIcon;
+    public Sprite secondaryRateUpIcon;
+    public string primaryRateUpName;
+
+    public Sprite poolVisual => primaryRateUpIcon;
 }
