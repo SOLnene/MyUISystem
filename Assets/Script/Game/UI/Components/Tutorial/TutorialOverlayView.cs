b3d018c54470827054389e14f5ce640e7a1e52ce
SOLnene <664621381@qq.com>
2026-08-13 00:35:21 +0800

    实现教程高亮边框收束动画

--- Assets/Script/Game/UI/Components/Tutorial/TutorialOverlayView.cs
+++ Assets/Script/Game/UI/Components/Tutorial/TutorialOverlayView.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using SkierFramework;
 using UnityEngine;
 using UnityEngine.UI;
@@ -17,12 +18,18 @@ public partial class TutorialOverlayView : UIView
     [SerializeField] RectTransform rightBlocker;
     [SerializeField] RectTransform focusRoot;
     [SerializeField] RectTransform focusFrame;
+    [SerializeField] RectTransform convergingFrame;
     [SerializeField] RectTransform messagePanel;
     [SerializeField] TextMeshProUGUI messageText;
     [SerializeField] float focusPadding = 16f;
+    [SerializeField] float convergingStartPadding = 120f;
+    [SerializeField] float convergingDuration = 0.45f;
 
     ITutorialOverlaySession session;
     RectTransform currentTarget;
+    Tween convergingTween;
+    Rect convergingStartRect;
+    float convergingProgress;
 
     public override void OnInit(UIControlData uiControlData,UIViewHandle handle)
     {
@@ -55,6 +62,7 @@ public partial class TutorialOverlayView : UIView
     public override void OnClose()
     {
         currentTarget = null;
+        StopConvergingAnimation();
         session?.Detach(this);
         session = null;
         base.OnClose();
@@ -62,12 +70,14 @@ public partial class TutorialOverlayView : UIView
 
     public override void OnRelease()
     {
+        StopConvergingAnimation();
         base.OnRelease();
     }
 
     internal void ShowMessageOnly(string message)
     {
         currentTarget = null;
+        StopConvergingAnimation();
         messageText.text = message;
         fullScreenDimmer.gameObject.SetActive(true);
         cutoutDimmerRoot.gameObject.SetActive(false);
@@ -77,18 +87,20 @@ public partial class TutorialOverlayView : UIView
 
     internal void FocusTarget(RectTransform target, string message)
     {
+        StopConvergingAnimation();
         currentTarget = target;
         messageText.text = message;
         fullScreenDimmer.gameObject.SetActive(false);
         cutoutDimmerRoot.gameObject.SetActive(true);
         focusRoot.gameObject.SetActive(true);
         messagePanel.gameObject.SetActive(true);
-        UpdateFocusRect();
+        PlayConvergingAnimation(UpdateFocusRect());
     }
 
     internal void HideGuidance()
     {
         currentTarget = null;
+        StopConvergingAnimation();
         fullScreenDimmer.gameObject.SetActive(false);
         cutoutDimmerRoot.gameObject.SetActive(false);
         focusRoot.gameObject.SetActive(false);
@@ -109,11 +121,16 @@ public partial class TutorialOverlayView : UIView
     {
         if (currentTarget != null && currentTarget.gameObject.activeInHierarchy)
         {
-            UpdateFocusRect();
+            Rect focusRect = UpdateFocusRect();
+            if (convergingTween != null && convergingTween.IsActive())
+            {
+                // 插值位置和尺寸而非缩放节点，避免九宫格边线随动画变粗或变细。
+                SetRect(convergingFrame, LerpRect(convergingStartRect, focusRect, convergingProgress));
+            }
         }
     }
 
-    void UpdateFocusRect()
+    Rect UpdateFocusRect()
     {
         Bounds bounds = RectTransformUtility.CalculateRelativeRectTransformBounds(
             pageRoot,
@@ -129,6 +146,56 @@ public partial class TutorialOverlayView : UIView
         SetRect(leftBlocker, parentRect.xMin, minY, minX, maxY);
         SetRect(rightBlocker, maxX, minY, parentRect.xMax, maxY);
         SetRect(focusFrame, minX, minY, maxX, maxY);
+        return Rect.MinMaxRect(minX, minY, maxX, maxY);
+    }
+
+    void PlayConvergingAnimation(Rect focusRect)
+    {
+        Rect parentRect = pageRoot.rect;
+        convergingStartRect = Rect.MinMaxRect(
+            Mathf.Max(parentRect.xMin, focusRect.xMin - convergingStartPadding),
+            Mathf.Max(parentRect.yMin, focusRect.yMin - convergingStartPadding),
+            Mathf.Min(parentRect.xMax, focusRect.xMax + convergingStartPadding),
+            Mathf.Min(parentRect.yMax, focusRect.yMax + convergingStartPadding));
+        convergingProgress = 0f;
+        convergingFrame.gameObject.SetActive(true);
+        SetRect(convergingFrame, convergingStartRect);
+
+        // 教程可能覆盖暂停时间的界面，因此收束动画不依赖游戏 timeScale。
+        convergingTween = DOTween.To(
+                () => convergingProgress,
+                value => convergingProgress = value,
+                1f,
+                convergingDuration)
+            .SetEase(Ease.OutCubic)
+            .SetUpdate(true)
+            .SetTarget(convergingFrame)
+            .OnComplete(() =>
+            {
+                convergingTween = null;
+                convergingFrame.gameObject.SetActive(false);
+            });
+    }
+
+    void StopConvergingAnimation()
+    {
+        convergingTween?.Kill();
+        convergingTween = null;
+        convergingFrame.gameObject.SetActive(false);
+    }
+
+    void SetRect(RectTransform rectTransform, Rect rect)
+    {
+        SetRect(rectTransform, rect.xMin, rect.yMin, rect.xMax, rect.yMax);
+    }
+
+    Rect LerpRect(Rect from, Rect to, float progress)
+    {
+        return Rect.MinMaxRect(
+            Mathf.Lerp(from.xMin, to.xMin, progress),
+            Mathf.Lerp(from.yMin, to.yMin, progress),
+            Mathf.Lerp(from.xMax, to.xMax, progress),
+            Mathf.Lerp(from.yMax, to.yMax, progress));
     }
 
     void SetRect(RectTransform rectTransform, float minX, float minY, float maxX, float maxY)
